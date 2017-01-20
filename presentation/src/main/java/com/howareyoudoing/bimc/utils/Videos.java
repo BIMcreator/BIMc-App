33cd7ec6c99285ffa0c07ccdd1186de3f6cab706
Ilya Gorbunov <iliuagorbunov@gmail.com>
2017-01-20 15:14:48 +0400

    complete presentation layer

--- presentation/src/main/java/com/howareyoudoing/bimc/utils/Videos.java
+++ presentation/src/main/java/com/howareyoudoing/bimc/utils/Videos.java
@@ -1,8 +1,24 @@
 package com.howareyoudoing.bimc.utils;
 
-/**
- * Created by davey on 29.05.2017.
- */
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+import com.howareyoudoing.data.model.content.Video;
+
 
 public class Videos {
+
+	private static final String YOUTUBE = "https://www.youtube.com/watch?v=";
+
+	private Videos() {
+	}
+
+	public static void browseVideo(@NonNull Context context, @NonNull Video video) {
+		String videoUrl = YOUTUBE + video.getKey();
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(videoUrl));
+		context.startActivity(intent);
+	}
+
 }
